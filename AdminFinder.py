82e18de366a367a0f0f019df1cd08eca88228fbd
spaddex <spaddex@protonmail.com>
2017-10-28 12:55:13 +0200

    Made it more user-friendly

--- AdminFinder.py
+++ AdminFinder.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 
+import sys
+import time
 import requests
 from threading import Thread
-import time
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,70 +16,102 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-baseurl = input("Enter base URL (with http/https): ")
-useragent = {
-	'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
+
+useragent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 pathArray = []  # Placeholder for our paths
 threadList = []  # Somewhere to put our threads
 positives = 0  # To keep track of how many matches we get
 foundPages = []
-inform=0
+inform = 0
 
 """Loads the paths into the pathArray"""
-def readLines():
-	with open("admin_locations.txt", 'r') as x:
-		paths=x.readlines()
-		for path in paths:
-			pathArray.append(str(path))
-		print("Loaded " + str(len(pathArray)) + " paths")
 
+
+def readLines():
+    with open("admin_locations.txt", 'r') as x:
+        paths = x.readlines()
+        for path in paths:
+            pathArray.append(str(path))
+        print("Loaded " + str(len(pathArray)) + " paths")
 
 
 def requester(completeURL):
-	global positives, htmlrequest, inform
-	try:
-		htmlrequest = requests.get(completeURL, headers=useragent, allow_redirects=False)
-		print(completeURL)
-		if htmlrequest.status_code == 200:
-			positives += 1
-			foundPages.append(completeURL)
+    global positives, htmlrequest, inform
+    try:
+        htmlrequest = requests.get(completeURL, headers=useragent, allow_redirects=False)
+        print(completeURL)
+        if htmlrequest.status_code == 200:
+            positives += 1
+            foundPages.append(completeURL)
+    except Exception as exp:
+        print(bcolors.WARNING + "Something failed: " + str(exp) + ". Missed this request: " + str(completeURL) + bcolors.ENDC)
 
-	except Exception as exp:
-		print(bcolors.WARNING + "Something failed: " + str(exp) + ". Missed this request: " + str(completeURL) + bcolors.ENDC)
+    if positives < 5 and (htmlrequest.status_code == 200):
+        print(bcolors.OKGREEN + "Found: " + str(completeURL) + bcolors.ENDC)
 
-	if positives < 5 and (htmlrequest.status_code == 200):
-		print(bcolors.OKGREEN + "Found: " + str(completeURL) + bcolors.ENDC)
+    elif positives > 5 and (inform == 0):
+        print(bcolors.FAIL + "Never mind, false positives. Send me a ctrl+c" + bcolors.ENDC)
+        inform += 1
 
-	elif positives > 5 and (inform == 0):
-		print(bcolors.FAIL + "Never mind, false positives. Send me a ctrl+c" + bcolors.ENDC)
-		inform += 1
 
+# Get input on commandline
+def getAttackURL():
+    if len(sys.argv) == 2:  # Check if we got input on commandline
+        target = str(sys.argv[1])
+        if(target.startswith('http')) == False:  # Check if schema was supplied
+            print(bcolors.WARNING + "HTTP / HTTPS not specified, assuming HTTP" + bcolors.ENDC)
+            target = ('http://'+str(target))
+        if(target[-1:]) == '/':  # if last char is / (http://example.com/)
+            return(target[:-1])  # return URL minus last char (http://example.com)
+        else:
+            return target
+    else:
+        target = askForAttackURL()  # fall over to asking for input
+        return target
+
+
+# Will ask the user for input instead
+def askForAttackURL():
+    target = input("Enter base URL (with http/https): ")
+    if (target.startswith('http')) == False:
+        print(bcolors.WARNING + "HTTP / HTTPS not specified, assuming HTTP" + bcolors.ENDC)
+        target = ('http://' + str(target))
+    if (target[-1:]) == '/':  # if last char is / (http://example.com/)
+        return (target[:-1])  # return URL minus last char (http://example.com)
+    else:
+        return target
 
 
 def main():
-	readLines()
-	try:
-		for path in pathArray:
-			t = Thread(target=requester, args=(baseurl + path.strip(),))
-			t.start()
-			time.sleep(.1)  # We can be somewhat gentle :)
-			threadList.append(t)
+    baseurl = getAttackURL()
+    readLines()
+    try:
+        for path in pathArray:
+            t = Thread(target=requester, args=(baseurl + path.strip(),))
+            t.start()
+            time.sleep(.1)  # We can be somewhat gentle :)
+            threadList.append(t)
+        for tlist in threadList:
+            t.join()
+    except KeyboardInterrupt:
+        print(bcolors.HEADER + "Shutting Down" + bcolors.ENDC)
+    except TypeError:
+        print(bcolors.FAIL + "Did you input an URL?" + bcolors.ENDC)
+        print(("Details: \n {} \n {} Line: {} \n" + bcolors.ENDC).format(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2].tb_lineno))
+        return 0
 
-		for tlist in threadList:
-			t.join()
-	except KeyboardInterrupt:
-		print( bcolors.HEADER + "Shutting Down" + bcolors.ENDC)
+    if positives == 0:
+        print(bcolors.FAIL + "Couldn't find any admin-page" + bcolors.ENDC)
+    elif positives > 6:
+        print(bcolors.WARNING + "Many hits, probably false positives" + bcolors.ENDC)
+        for page in foundPages:
+            print(str(page))
+    elif positives < 6:
+        print(bcolors.OKBLUE + " Found pages: " + bcolors.ENDC)
+        for page in foundPages:
+            print("  " + str(page.strip()))
 
-	if positives == 0:
-		print(bcolors.FAIL + "Couldn't find any admin-page" + bcolors.ENDC)
-	elif positives > 6:
-		print(bcolors.WARNING + "Many hits, probably false positives" + bcolors.ENDC)
-		for page in foundPages:
-			print(str(page))
-	elif positives < 6:
-		print(bcolors.OKBLUE+ " Found pages: ")
-		for page in foundPages:
-			print("  " + str(page.strip()))
 
 if __name__ == "__main__":
-	main()
+    main()
